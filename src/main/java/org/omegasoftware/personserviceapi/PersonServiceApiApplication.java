06d3bfd5caecb7b401193d49ed7d9a1e05959a6d
MarcelloTinoco <marcellojorge@gmail.com>
2019-07-06 13:51:30 -0300

    initial commit

---
+++ src/main/java/org/omegasoftware/personserviceapi/PersonServiceApiApplication.java
@@ -0,0 +1,13 @@
+package org.omegasoftware.personserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonServiceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/omegasoftware/personserviceapi/PersonServiceApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.omegasoftware.personserviceapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PersonServiceApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
